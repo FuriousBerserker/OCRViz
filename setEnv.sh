750063d21e380bfb2e6d356f7876a9a7e700d4a1
FuriousBerserker <ylcmen@gmail.com>
2017-12-03 20:16:15 -0500

    add files

---
+++ setEnv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export OCR_INSTALL=/home/berserker/main/workspace/Research/OCRDataRaceDetection/OCRDebugRuntime/install
+export LD_LIBRARY_PATH=/home/berserker/main/workspace/Research/OCRDataRaceDetection/OCRDebugRuntime/install/lib:/home/berserker/main/src/ocr-1.1/xstg/apps/apps/libs/install/x86/lib:/usr/local/cuda-7.5/lib64:
+export OCR_CONFIG=/home/berserker/main/workspace/Research/OCRDataRaceDetection/OCRViz/race-detect.cfg
+export CONFIG_NUM_THREADS=1
+export OCR_TYPE=x86
